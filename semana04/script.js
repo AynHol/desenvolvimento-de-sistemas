68aa1e316024eb83f96a59160a692ca39187b572
WESLEY ALESSANDRO DARÉ ANTUNES <wesley_a_antunes@estudante.sesisenai.org.br>
2025-02-24 21:57:03 -0300

    semana 04

---
+++ semana04/script.js
@@ -0,0 +1,23 @@
+async function loadProfile() {
+    const username = document.getElementById("username").value;
+    if (!username) {
+        alert("Você Precisa Digitar Um Usuário!!!");
+        return;
+    }
+
+    const response = await fetch(`https://api.github.com/users/${username}`, {
+        method: "GET",
+    });
+    const user = await response.json();
+
+    const profileElement = document.createElement("div");
+    profileElement.classList.add("profile-item");
+    profileElement.innerHTML = `
+    <img src='${user.avatar_url}' />
+    <h2>${user.name || user.login}</h2>
+    <p>${user.bio}</p>
+    `;
+
+    const profiles = document.getElementById("profiles");
+    profiles.appendChild(profileElement);
+}
