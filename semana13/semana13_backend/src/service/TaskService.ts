d0949de0b1af409442e9a4c7037b30f4477a114b
WESLEY ALESSANDRO DARÉ ANTUNES <wesley_a_antunes@estudante.sesisenai.org.br>
2025-05-12 19:35:03 -0300

    prisma

--- semana13/semana13_backend/src/service/TaskService.ts
+++ semana13/semana13_backend/src/service/TaskService.ts
@@ -1,56 +1,42 @@
-import { Task } from "../entity/Task";
-// import { TaskRepository } from "../repository/TaskRepository";
+import { Task as PTASK } from "@prisma/client";
+import { prisma } from "../prisma/client";
 
 class TaskService {
-    private taskList: Task[] = [];
-    // private taskRepository = new TaskRepository();
 
-    public create(text: string): void {
-        const textAlreadyExist = this.taskList.find((task) => task.getText() === text);
-        if (textAlreadyExist) {
-            throw new Error("There is already a task with this text.");
-        }
-        const newTask = new Task(text);
-        // this.taskRepository.save(newTask);
-        this.taskList.push(newTask);
+    public async create(text: string): Promise<void> {
+        const task: PTASK = {
+            id: crypto.randomUUID(),
+            text: text,
+            completed: false,
+            createdAt: new Date(),
+            updatedAr: new Date(),
+        };
+        await prisma.task.create({ data: task });
     }
 
-    public async getAll() {
-        // const task = await this.taskRepository.findAll();
-        return this.taskList;
+    public async getAll(): Promise<PTASK[]> {
+        return await prisma.task.findMany();
     }
 
-    public getById(id: string): Task | null {
-        // const task = this.taskList.filter((task) => task.getId() === id);
-        // return task.lenght ? task[0] : null;
-
-        // const task = this.taskList.findIndex((task) => task.getId() === id);
-        // return index >= 0 ? this.taskList(index) : null;
-
-        const tasks = this.taskList.find((task) => task.getId() === id);
-        return !tasks ? null : tasks;
-    }
-
-    public updateCompleted(id: string) {
-        const task = this.getById(id);
-        if (task === null) {
+    public async updateCompleted(id: string) {
+        const task = await prisma.task.findUnique({ where: { id } });
+        if (task == null) {
             throw new Error("Task not found.");
         }
-        task.setCompleted();
-        return task;
+
+        const taskUpdate = {
+            completed: !task.completed,
+            updatedAt: new Date(),
+        };
+
+        return await prisma.task.update({
+            where: { id },
+            data: taskUpdate,
+        });
     }
 
-    public updateText(id: string, text: string) {
-        const task = this.getById(id);
-        if (task === null) {
-            throw new Error("Task not found.");
-        }
-        task.setText(text);
-        return task;
-    }
-
-    public deleteTask(id: string) {
-        this.taskList = this.taskList.filter((task) => task.getId() !== id);
+    public async deleteTask(id: string) {
+        return await prisma.task.delete({ where: { id } });
     }
 }
 
