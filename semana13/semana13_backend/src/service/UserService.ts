0fd9be6d1be51177924cec78627e59cd9999102f
WESLEY ALESSANDRO DARÉ ANTUNES <wesley_a_antunes@estudante.sesisenai.org.br>
2025-05-12 21:56:35 -0300

    semana 15

---
+++ semana13/semana13_backend/src/service/UserService.ts
@@ -0,0 +1,46 @@
+import { User } from "@prisma/client";
+import { prisma } from "../prisma/client";
+import { compare, hash } from "bcryptjs";
+
+class UserService {
+    public async register({ name, email, password, birthDate }: CreateUserType): Promise<void> {
+        const userExist = await prisma.user.findUnique({ where: { email: email } });
+        if (userExist) {
+            throw new Error("Email Already Registered!");
+        }
+
+        const passwordHashed = await hash(password, 10);
+
+        const user: User = {
+            id: crypto.randomUUID(),
+            name,
+            email,
+            password: passwordHashed,
+            birthDate: new Date(birthDate),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        };
+        await prisma.user.create({ data: user });
+    }
+
+    public async login({ email, password }: LoginType): Promise<string | null> {
+        const user = await prisma.user.findUnique({ where: { email: email } });
+        if (!user) {
+            throw new Error("Invalid Credentials ");
+        }
+
+        const passwordIsValid = await compare(password, user.password);
+        if (!passwordIsValid) {
+            throw new Error("Invalid Credentials ");
+        }
+
+        return app.jwt.sign({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            birthDate: user.birthDate,
+        });
+    }
+}
+
+export const userService = new UserService();
